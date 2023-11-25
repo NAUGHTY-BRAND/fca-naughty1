2a71b0e1df4ecec2f9ef6d20e2bc050e5a2c5c09
ÑAUGHTY BRAND <145090660+NAUGHTY-BRAND@users.noreply.github.com>
2023-11-25 00:26:05 -0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -869,9 +869,9 @@ function getAdminTextMessageType(m) {
  */
 
 function getGenderByPhysicalMethod(name) {
-    var GirlName = ["LAN", "HÂN", "LINH", "MAI", "HOA", "THU", "BĂNG", "MỸ", "CHÂU", "THẢO", "THOA", "MẪN", "THÙY", "THỦY", "NGA", "NGÂN", "NGHI", "THƯ", "NGỌC", "BÍCH", "VÂN", "DIỆP", "CHI", "TIÊN", "XUÂN", "GIANG", "NHUNG", "DUNG", "NHƯ", "YẾN", "QUYÊN", "YẾN", "TƯỜNG", "VY", "PHƯƠNG", "LIÊN", "LAN", "HÀ", "MAI", "ĐAN", "HẠ", "QUYÊN", "LY", "HÒA", "OANH", "HƯƠNG", "HẰNG", "QUỲNH", "HẠNH", "NHIÊN", "NHẠN"];
+    var GirlName = ["PRIYANSHI", "VANSHI", "TANIYA", "KAVYA", "AASHIKA", "AKSHITA", "ANSHIKA", "ANUSHKA", "ANKITA", "NEHA", "PARI", "PRIYA", "PRITI", "SANIA", "SONA", "SONIA", "MARRIYAM", "MAMTA", "ANU", "LOVELY", "VISHAKA", "DIYA", "DEEPIKA", "KIRAN", "POONAM", "SANA", "NAUGHTY", "ZARA", "AARTI", "QUEEN", "ANEE", "SAFIYA", "MAHI", "JOYA", "ALIKA", "ZOYA", "JASMINE", "BABY", "ZAIFA", "RANI", "BACHI", "SHALU", "LY", "HÒA", "OANH", "HƯƠNG", "HẰNG", "QUỲNH", "HẠNH", "NHIÊN", "NHẠN"];
 
-    var BoyName = ["HƯNG", "HUY", "KHẢI", "KHANG", "KHOA", "KHÔI", "KIÊN", "KIỆT", "LONG", "MINH", "ÂN", "BẢO", "BÌNH", "CƯỜNG", "ĐẠT", "ĐỨC", "DŨNG", "DUY", "HOÀNG", "HÙNG", "HƯNG", "NGHĨA", "NGUYÊN", "THẮNG", "THIỆN", "THỊNH", "TÒA", "TRIẾT", "TRUNG", "TRƯỜNG", "TUẤN", "NHÂN", "VŨ", "VINH", "PHONG", "PHÚC", "QUÂN", "QUANG", "SƠN", "TÀI", "THẮNG", "ĐĂNG", "VĂN", "VĨ", "QUANG", "MẠNH"];
+    var BoyName = ["PRIYANSHU", "PRIYANSH", "PRINCE", "AAKASH", "CHINMAY", "MANISH", "AWAIS", "PINTU", "ABHISHEK", "KUSHAL", "VIKAS", "SHIVANSH", "ARYAN", "ARUN", "AADI", "PANDA", "AJAY", "USMAN", "TERA", "FAISAL", "KHAN", "RAJPUT", "AAKASH", "AHMAD", "RAMESH", "THỊNH", "TÒA", "TRIẾT", "TRUNG", "TRƯỜNG", "TUẤN", "NHÂN", "VŨ", "VINH", "PHONG", "PHÚC", "QUÂN", "QUANG", "SƠN", "TÀI", "THẮNG", "ĐĂNG", "VĂN", "VĨ", "QUANG", "YASH"];
 
     var OtherName = ["ANH", "THANH", "TÂM", "DƯƠNG", "AN", "LÂM", "MIÊN", "TÚ", "LÂM", "BẰNG", "KHÁNH", "NHẬT", "VỸ", ".",",","/","%", "&","*","-","+"];
 
@@ -1361,7 +1361,7 @@ function parseAndCheckLogin(ctx, defaultFuncs, retryCount) {
             }
 
             if (res.error === 1357001) {
-                switch (globalThis.Fca.Require.FastConfig.AutoLogin) {
+                switch (globalThis.Fca.Require.Priyansh.AutoLogin) {
                     case true: {
                         globalThis.Fca.Require.logger.Warning(globalThis.Fca.Require.Language.Index.AutoLogin, function() {
                             return globalThis.Fca.AutoLogin();
@@ -1572,7 +1572,7 @@ function getAppState(jar, Encode) {
     var Security = require('./Extra/Security/Index');
     var appstate = jar.getCookies("https://www.facebook.com").concat(jar.getCookies("https://facebook.com")).concat(jar.getCookies("https://www.messenger.com"))
     var logger = require('./logger'),languageFile = require('./Language/index.json');
-    var Language = languageFile.find(i => i.Language == globalThis.Fca.Require.FastConfig.Language).Folder.Index;
+    var Language = languageFile.find(i => i.Language == globalThis.Fca.Require.Priyansh.Language).Folder.Index;
     var data;
         switch (require(process.cwd() + "/FastConfigFca.json").EncryptFeature) {
             case true: {
